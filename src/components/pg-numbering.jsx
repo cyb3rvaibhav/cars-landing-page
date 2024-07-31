f0bf2db1feb2232a411dbfaff53747cd4c885239
cyb3rvaibhav <cyb3rvaibhav@gmail.com>
2024-07-31 00:47:57 -0700

    folders formatted

---
+++ src/components/pg-numbering.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../App.css'
+
+function Pgnumbering(){
+    return (
+       <>
+       <div className="pg-numbering">
+        <li>01</li>
+        <a href="#"><li>02</li></a>
+        <li>03</li>
+        <li>04</li>
+        <li>05</li>
+      </div>
+      </>
+    )
+}
+export default Pgnumbering;
